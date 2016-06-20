855fa33226064efb9b75b10246553f0921445122
Matevz Vucnik <vucnikm@gmail.com>
2016-06-20 18:15:41 +0200

    add videk status indicator support

---
+++ videk-ping.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+PING=`ansible 'all:!local' -m ping | grep "=>"`
+
+NODES=(`echo "$PING" | cut -d' ' -f1,3 --output-delimiter=';'`)
+
+if [ "${#NODES[@]}" -eq 0 ]; then
+    echo "Must be more than 0 nodes!"
+    exit
+fi
+
+for NODE in "${NODES[@]}"; do
+    NAME=`echo $NODE | cut -d';' -f1`
+    STATUS=`echo $NODE | cut -d';' -f2`
+    NODE=`curl -s -X GET "http://localhost/api/nodes/?name=$NAME"`
+    ID="$(echo "$NODE" | grep -Po '"_id":(\d*?,|.*?[^\\]")' | cut -d'"' -f4)"
+
+    if [ "$STATUS" = "SUCCESS" ]; then
+      echo "--------active"
+      echo "$NAME"
+      echo "$STATUS"
+      curl -s -H "Content-Type: application/json" -X PUT -d \
+      '{"status":"active"}' "http://localhost/api/nodes/$ID"
+    else
+      echo "--------inactive"
+      echo "$NAME"
+      echo "$STATUS"
+      curl -s -H "Content-Type: application/json" -X PUT -d \
+      '{"status":"inactive"}' "http://localhost/api/nodes/$ID"
+    fi
+    echo $'\n'
+done
